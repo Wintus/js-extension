d34aa82c01ab56fd0346dd8e4e0d1094a5364a58
wintus <yuya0726+GitHub@gmail.com>
2019-10-10 02:06:29 +0900

    Fix type cast

--- test/object.test.ts
+++ test/object.test.ts
@@ -8,13 +8,15 @@ type ValueOf<T extends {}> = T[keyof T];
 
 test(`{foo: 0} ==> {FOO: 1}`, () => {
   const o = { foo: 0 };
-  const f = ([key, value]: [string, ValueOf<typeof o>]) =>
-    <[string, number]>[key.toUpperCase(), value + 1];
+  const f = ([key, value]: [string, ValueOf<typeof o>]): [string, number] => [
+    key.toUpperCase(),
+    value + 1,
+  ];
   expect(o.map(f)).toStrictEqual({ FOO: 1 });
 });
 
 test(`{foo: 0} ==> {foo1: 1}`, () => {
   const o = { foo: 0 };
-  const f = ([key, value]: [string, ValueOf<typeof o>]) => <[string, number]>[key + 1, value + 1];
+  const f = ([key, value]: [string, ValueOf<typeof o>]): [string, number] => [key + 1, value + 1];
   expect(o.map(f)).toStrictEqual({ foo1: 1 });
 });
