77d92ac1c84e8edb7d20774f94c1438cc86d636e
wintus <yuya0726+GitHub@gmail.com>
2019-10-10 02:08:28 +0900

    Refactor helper types

--- test/object.test.ts
+++ test/object.test.ts
@@ -8,15 +8,14 @@ type ValueOf<T extends {}> = T[keyof T];
 
 test(`{foo: 0} ==> {FOO: 1}`, () => {
   const o = { foo: 0 };
-  const f = ([key, value]: [string, ValueOf<typeof o>]): [string, number] => [
-    key.toUpperCase(),
-    value + 1,
-  ];
+  type Foo = [string, ValueOf<typeof o>];
+  const f = ([key, value]: Foo): Foo => [key.toUpperCase(), value + 1];
   expect(o.map(f)).toStrictEqual({ FOO: 1 });
 });
 
 test(`{foo: 0} ==> {foo1: 1}`, () => {
   const o = { foo: 0 };
-  const f = ([key, value]: [string, ValueOf<typeof o>]): [string, number] => [key + 1, value + 1];
+  type Foo = [string, ValueOf<typeof o>];
+  const f = ([key, value]: Foo): Foo => [key + 1, value + 1];
   expect(o.map(f)).toStrictEqual({ foo1: 1 });
 });
